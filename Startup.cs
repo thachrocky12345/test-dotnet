a7388cc3c95bd9dac63141db7bd2a9924fb3ddf4
ThachDfinity <75273681+ThachDfinity@users.noreply.github.com>
2021-01-19 11:29:17 -0600

    run on master

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace test_dotnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello DEV World! self host");
+                    await context.Response.WriteAsync("Hello DEV World! self host 1");
                 });
             });
         }
